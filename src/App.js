8bdc418dcf6227d7244e18cff2558667cfebb105
ruyi <tan_ruyi@hotmail.com>
2023-01-08 18:32:26 +0800

    created Character component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import placeholder_image from "./assets/elementor-placeholder-image.webp"
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
+import placeholder_image from "./assets/elementor-placeholder-image.webp";
+import Character from "./components/Character"
 
 function App() {
-  return (
-      <>
-          <h1 className="web_header">Voting Battle</h1>
-          <div className="voting_image_section">
-              <img className="voting_image" src={placeholder_image} alt="placeholder-image" />
-              <h2 className="vs_text">vs</h2>
-              <img className="voting_image" src={placeholder_image} alt="placeholder-image" />
-          </div>
-    </>
-  );
+	return (
+		<>
+			<h1 className="web_header">Voting Battle</h1>
+			<div className="voting_image_section">
+				<Character />
+				<h2 className="vs_text">vs</h2>
+				<Character />
+			</div>
+		</>
+	);
 }
 
 export default App;
