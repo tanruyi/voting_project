e38e9e197ff7376eefd22c471c83e6611c392c45
mingxxv <mingxxv@protonmail.ch>
2023-01-08 19:03:04 +0800

    oh lord

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import FrontPageList from './components/FrontPageList';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+<FrontPageList />
     </div>
   );
 }
--- src/components/FrontPageList.jsx
+++ src/components/FrontPageList.jsx
@@ -1,43 +1,33 @@
 import * as React from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import { ListItemButton } from "@mui/material";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import Box from "@mui/material/Box";
 
 const FrontPageList = () => {
   return (
     <>
       <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-        <nav>
-          <List>
-            <ListItem>
-              <ListItemButton>
-                <ListItemText primary="never" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem>
-              <ListItemButton>
-                <ListItemText primary="gonna" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem>
-              <ListItemButton>
-                <ListItemText primary="give" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem>
-              <ListItemButton>
-                <ListItemText primary="you" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem>
-              <ListItemButton>
-                <ListItemText primary="up" />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </nav>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          
+        </TableBody>
+      </Table>
+    </TableContainer>
       </Box>
     </>
   );
