2375aa6e602a37828ed473da2eb8a94c716d19f6
ruyi <tan_ruyi@hotmail.com>
2023-01-08 19:10:44 +0800

    replaced old code in app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import FrontPageList from './components/FrontPageList';
+/** @format */
+
+import "./App.css";
+import Character from "./components/Character";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-<FrontPageList />
-    </div>
-  );
+	return (
+		<>
+			<h1 className="web_header">Voting Battle</h1>
+			<div className="voting_image_section">
+				<Character />
+				<h1 className="vs_text">VS</h1>
+				<Character />
+			</div>
+		</>
+	);
 }
 
 export default App;
