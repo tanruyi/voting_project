01da1bcebccc8b2373d9ffc60c61292fe8abdb84
chris republic <christenrep@gmail.com>
2023-01-08 19:15:09 +0800

    wallet info

---
+++ src/Wallet.jsx
@@ -0,0 +1,57 @@
+import React, { useMemo, FC, PropsWithChildren } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton, 
+} from "@solana/wallet-adapter-react-ui";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+
+// Default styles that can be overridden by your app
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+
+export const Wallet = ({ children }) => {
+    
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+  const endpoint = useMemo(
+    () => clusterApiUrl(network),
+    [network]
+  );
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking --
+  // Only the wallets you configure here will be compiled into your application
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+    //   new SlopeWalletAdapter(),
+    //   new TorusWalletAdapter(),
+    //   new LedgerWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+            <WalletMultiButton />
+                {children}
+            </WalletModalProvider>
+        </WalletProvider>
+    </ConnectionProvider>
+  );
+};
