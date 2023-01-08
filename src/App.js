c22ece738c3fe4e121aa3554fc1927a37a819579
ruyi <tan_ruyi@hotmail.com>
2023-01-08 18:26:51 +0800

    added image section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import placeholder_image from "./assets/elementor-placeholder-image.webp"
 
 function App() {
   return (
       <>
           <h1 className="web_header">Voting Battle</h1>
+          <div className="voting_image_section">
+              <img className="voting_image" src={placeholder_image} alt="placeholder-image" />
+              <h2 className="vs_text">vs</h2>
+              <img className="voting_image" src={placeholder_image} alt="placeholder-image" />
+          </div>
     </>
   );
 }
